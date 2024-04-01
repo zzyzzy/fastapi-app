f0a7ff5828e79e8f754d1e6f620001db8357fd8f
zzyzzy <zzyzigy@gmail.com>
2024-04-01 23:39:06 +0900

    db

---
+++ db/init-db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+    id int primary key,
+    name varchar(50),
+    email varchar(120)
+);
+
+INSERT INTO users VALUES(1, 'abc123', 'abc123@abc123.co.kr');
+INSERT INTO users VALUES(2, '987xyz', '987xyz@987xyz.co.kr');
+
