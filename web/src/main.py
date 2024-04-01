f94f7fde0b71e9593fe4585caac7db817e919608
zzyzzy <zzyzigy@gmail.com>
2024-04-01 23:43:42 +0900

    Update main.py

--- web/src/main.py
+++ web/src/main.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+import os
 
 app = FastAPI()
 
